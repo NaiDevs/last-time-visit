db13303e8d8e3227f95274264d3d93447508acf6
Naidelyn Maldonado <naidelynmaldonado54@gmail.com>
2024-05-15 22:24:39 -0600

    adding animation

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'https://deno.land/x/hono@v4.3.7/mod.ts'
-import { serveStatic } from 'https://deno.land/x/hono@v4.3.7/middleware.ts'
+import { cors, serveStatic } from 'https://deno.land/x/hono@v4.3.7/middleware.ts'
 import { streamSSE } from 'https://deno.land/x/hono@v4.3.7/helper/streaming/index.ts'
 
 const db = await Deno.openKv()
@@ -11,6 +11,8 @@ interface LastVisit {
     city: string,
     flag: string,
 }
+
+app.use(cors())
 
 app.get('/', serveStatic({ path: './index.html' }))
 
